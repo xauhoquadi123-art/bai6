442b732c51f76979f636fd1ba587443377b3f625
xauhoquadi123-art <xauhoquadi123@gmail.com>
2026-03-13 11:09:58 +0700

    bai6.baitap6.1.js

---
+++ bai6.baitap6.1.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    alert('heloo!')
+}
+
+function showSuccess() {
+    document.getElementById('messageDiv').innerHTML = 'Hello!'
+}
+
+function showError() {
+
+    console.log('Xin chào!')
+
+}
+function showInfo() {
+ document.write('Hello') }
